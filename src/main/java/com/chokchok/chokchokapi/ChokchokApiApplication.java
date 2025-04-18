004d917f86da72037d03ed53486269624fa9407a
joo <helloworld.v7@gmail.com>
2025-04-18 19:34:57 +0900

    프로젝트 생성

---
+++ src/main/java/com/chokchok/chokchokapi/ChokchokApiApplication.java
@@ -0,0 +1,13 @@
+package com.chokchok.chokchokapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChokchokApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChokchokApiApplication.class, args);
+    }
+
+}
