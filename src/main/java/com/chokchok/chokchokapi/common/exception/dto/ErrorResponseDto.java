e5cea06a23b540acfc5ed99f949155a3f4ffe15e
joo <helloworld.v7@gmail.com>
2025-04-25 19:42:34 +0900

    ErrorResponseDto

--- src/main/java/com/chokchok/chokchokapi/common/exception/dto/ErrorResponseDto.java
+++ src/main/java/com/chokchok/chokchokapi/common/exception/dto/ErrorResponseDto.java
@@ -5,19 +5,23 @@ import java.time.LocalDateTime;
 /**
  * 에러 응답 정보를 담는 DTO 클래스
  *
- * @param status    HTTP 상태 코드
- * @param message   에러 메시지
- * @param timestamp 에러 발생 시간
+ * @param error           에러 발생 여부 (true/false)
+ * @param httpStatusCode  HTTP 상태 코드 (예: 400, 404 등)
+ * @param errorCode       사용자 지정 에러 코드 (ErrorCode에서 정의된 값)
+ * @param message         에러 메시지
+ * @param timestamp       에러 발생 시간
  */
 
 public record ErrorResponseDto(
-        int status,
+        boolean error,
+        int httpStatusCode,
+        int errorCode,
         String message,
         LocalDateTime timestamp
 ) {
 
-    public static ErrorResponseDto of(int status, String message) {
-        return new ErrorResponseDto(status, message, LocalDateTime.now());
+    public static ErrorResponseDto of(int httpStatusCode, int errorCode, String message) {
+        return new ErrorResponseDto(true, httpStatusCode, errorCode, message, LocalDateTime.now());
     }
 
 }
