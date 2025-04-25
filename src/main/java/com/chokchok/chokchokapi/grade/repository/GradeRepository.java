c9b6aa568b642db39073000a24e7b0f3720a0f00
joo <helloworld.v7@gmail.com>
2025-04-25 19:42:19 +0900

    공통 예외 처리 방법 수정

--- src/main/java/com/chokchok/chokchokapi/grade/repository/GradeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chokchok.chokchokapi.grade.repository;
-
-import com.chokchok.chokchokapi.grade.domain.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Integer> {
-}
--- src/main/java/com/chokchok/chokchokapi/member/exception/InvalidStatusValueException.java
+++
@@ -1,9 +0,0 @@
-package com.chokchok.chokchokapi.member.exception;
-
-import com.chokchok.chokchokapi.common.exception.base.InvalidEnumValueException;
-
-public class InvalidStatusValueException extends InvalidEnumValueException {
-    public InvalidStatusValueException(String message) {
-        super(message);
-    }
-}
