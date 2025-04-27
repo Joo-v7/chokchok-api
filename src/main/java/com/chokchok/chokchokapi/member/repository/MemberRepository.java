edfbbb19df9e5a0b8b047ac7eae77005b7edc691
joo <helloworld.v7@gmail.com>
2025-04-28 01:18:47 +0900

    email로 Member 가져오는 쿼리 메서드 추가

--- src/main/java/com/chokchok/chokchokapi/member/repository/MemberRepository.java
+++ src/main/java/com/chokchok/chokchokapi/member/repository/MemberRepository.java
@@ -3,7 +3,10 @@ package com.chokchok.chokchokapi.member.repository;
 import com.chokchok.chokchokapi.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsMemberByUsername(String username);
     boolean existsMemberByEmail(String email);
+    Optional<Member> findMemberByEmail(String email);
 }
