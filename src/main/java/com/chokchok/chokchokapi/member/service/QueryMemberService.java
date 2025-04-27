bc766a8d2122df1add7c5ff1fead35ac02bc12aa
joo <helloworld.v7@gmail.com>
2025-04-28 01:20:08 +0900

    로그인에 필요한 정보를 가져오는 로직 추가

--- src/main/java/com/chokchok/chokchokapi/member/service/QueryMemberService.java
+++ src/main/java/com/chokchok/chokchokapi/member/service/QueryMemberService.java
@@ -3,6 +3,7 @@ package com.chokchok.chokchokapi.member.service;
 import com.chokchok.chokchokapi.common.exception.base.NotFoundException;
 import com.chokchok.chokchokapi.common.exception.code.ErrorCode;
 import com.chokchok.chokchokapi.member.domain.Member;
+import com.chokchok.chokchokapi.member.dto.response.MemberLoginResponseDto;
 import com.chokchok.chokchokapi.member.dto.response.MemberResponseDto;
 import com.chokchok.chokchokapi.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,14 +22,26 @@ public class QueryMemberService {
     private final MemberRepository memberRepository;
 
     /**
+     * 이메일로 로그인에 필요한 정보 조회
+     * @param email
+     * @return MemberLoginResponseDto
+     */
+    @Transactional(readOnly = true)
+    public MemberLoginResponseDto findMemberLoginInfoByEmail(String email) {
+        Member member = memberRepository.findMemberByEmail(email)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND, "Member Not Found with email: {}" + email));
+        return MemberLoginResponseDto.from(member);
+    }
+
+    /**
      * 회원 조회 by id(P.K.)
-     * @param id
+     * @param memberId
      * @return MemberResponseDto
      */
     @Transactional(readOnly = true)
-    public MemberResponseDto findMemberById(Long id) {
-        Member member = memberRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND, "Member Not Found with: {}" + id));
+    public MemberResponseDto findMemberById(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND, "Member Not Found with id: {}" + memberId));
         return MemberResponseDto.from(member);
     }
 
