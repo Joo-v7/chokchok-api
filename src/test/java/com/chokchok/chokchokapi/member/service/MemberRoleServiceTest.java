763fc847da4d0d16086e2f4e18e7e0415c85ccbb
joo <helloworld.v7@gmail.com>
2025-05-10 23:23:05 +0900

    member role service test

---
+++ src/test/java/com/chokchok/chokchokapi/member/service/MemberRoleServiceTest.java
@@ -0,0 +1,39 @@
+package com.chokchok.chokchokapi.member.service;
+
+import com.chokchok.chokchokapi.common.exception.base.NotFoundException;
+import com.chokchok.chokchokapi.member.repository.MemberRoleRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class MemberRoleServiceTest {
+
+    @InjectMocks
+    private MemberRoleService memberRoleService;
+
+    @Mock
+    private MemberRoleRepository memberRoleRepository;
+
+    @Test
+    @DisplayName("Default MemberRole을 가져오지 못하면 예외가 발생한다.")
+    void fail_getDefaultMemberRoleEntity() {
+        // when
+        Mockito.when(memberRoleRepository.findByName(Mockito.anyString())).thenReturn(Optional.empty());
+
+        // then
+        Assertions.assertThrows(NotFoundException.class,
+                () -> memberRoleService.getDefaultMemberRoleEntity());
+
+    }
+
+}
