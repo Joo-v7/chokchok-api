2065cfc40fffe252bec76a5c0781b43746deaf7d
joo <helloworld.v7@gmail.com>
2025-05-15 23:38:29 +0900

    product image entity

---
+++ src/main/java/com/chokchok/chokchokapi/product/domain/ProductImage.java
@@ -0,0 +1,51 @@
+package com.chokchok.chokchokapi.product.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "product_images")
+@Entity
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private ProductImage(Product product, String url) {
+        this.product = product;
+        this.url = url;
+    }
+
+    /**
+     * 상품 이미지를 생성하는 정적 팩토리 메소드
+     * @param product
+     * @param url 이미지
+     * @return ProductImage
+     */
+    public static ProductImage create(Product product, String url) {
+        return new ProductImage(product, url);
+    }
+
+}
