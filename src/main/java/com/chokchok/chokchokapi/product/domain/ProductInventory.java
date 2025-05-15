5b6df94792b55a789e4b6898e4f382a16473e5d0
joo <helloworld.v7@gmail.com>
2025-05-15 23:38:37 +0900

    product inventory entity

---
+++ src/main/java/com/chokchok/chokchokapi/product/domain/ProductInventory.java
@@ -0,0 +1,79 @@
+package com.chokchok.chokchokapi.product.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "product_inventories")
+@Entity
+public class ProductInventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private boolean isSoldOut;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private ProductInventory(Product product, Integer quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    /**
+     * 상품 재고를 생성하는 정적 팩토리 메소드
+     * @param product
+     * @param quantity
+     * @return ProductInventory
+     */
+    public static ProductInventory create(Product product, Integer quantity) {
+        return new ProductInventory(
+                product,
+                quantity
+        );
+    }
+
+    /**
+     * 상품 재고 수량을 업데이트합니다.
+     * @param quantity
+     */
+    public void updateQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * 상품 재고 매진 여부를 업데이트합니다.
+     * @param isSoldOut
+     */
+    public void updateSoldOut(boolean isSoldOut) {
+        this.isSoldOut = isSoldOut;
+    }
+
+    /**
+     * 상품 업데이트 일시를 업데이트합니다.
+     */
+    public void updateUpdatedAt() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+
+}
+
